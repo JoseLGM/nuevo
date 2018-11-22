77401b1033128a1777714d58718970af52988783
JoseLGM <jose.lm956@gmail.com>
2018-11-22 10:18:49 -0600

    initial commit

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 var a=2;
 var b=3;
 var c=a+b;
-consele.log(c);
+console.log(c);
+var cadena="hola mundo";
+var vcad=cadena.split("");
+for(var i=0;i<cadena.length;i++)
+{
+  console.log(vcad[i]);
+}
