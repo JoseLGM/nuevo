a866b25c9af91de7977c6332aa9307b97ae6f35e
JoseLGM <jose.lm956@gmail.com>
2018-11-23 00:56:19 -0600

    consultas

--- index.js
+++ index.js
@@ -8,3 +8,25 @@ for(var i=0;i<cadena.length;i++)
 {
   console.log(vcad[i]);
 }
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+ host     : 'localhost',
+ user     : 'root',
+ password : 'vane',
+ database : 'classicmodels'
+});
+
+
+connection.connect();
+connection.query('show tables',function(error,results,fields)
+{
+  if(error) throw error;
+  console.log(results);
+
+   for (var i=0;i<results.length;i++)
+   {
+      console.log("tabla numero"+(i+1))+
+      results[i].Tables_in_classicmodels);
+   }
+}
+connection.end;
